b93ece4a0b58c5e8d5af79b4d2e2da1666f0df2e
Madison Henry <98718577+Madison-Data@users.noreply.github.com>
2022-06-26 17:49:18 -0400

    file upload

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://127.0.0.1:27017/mars_mission")
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars_data = scrape_mars.scrape()
+    mongo.db.mars.update({}, mars_data, upsert = True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
